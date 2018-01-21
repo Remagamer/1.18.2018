e29bc6de1134040d233d287c67c974e1e572f1b6
Unknown <Remagstudios@gmail.com>
2018-01-20 22:03:29 -0700

    Here they are....
    Weird they didn't show up when they already exist.

---
+++ test/Assets/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("I love my cat.");
+        print("No, I really don't.");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+}
